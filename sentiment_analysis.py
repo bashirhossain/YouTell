657ece71566d3df40983f7b1dbf60a4b928460fa
bashirhossain <bashirhossain@iut-dhaka.edu>
2022-05-19 05:03:42 +0600

    final

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,7 +8,7 @@ def google_api(id):
 
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = "<your-developer-key>"
+    DEVELOPER_KEY = "AIzaSyAKoqZWKYbpYfl5fMUjKIaKIdIi8LaehUA"
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
@@ -83,14 +83,7 @@ df = remove_comments(df)
 from textblob.blob import TextBlob
 
 
-def find_subjectivity_on_single_comment(text):
-  return TextBlob(text).sentiment.subjectivity
-   
-def apply_subjectivity_on_all_comments(df):
-  df['Subjectivity'] = df['Comments'].apply(find_subjectivity_on_single_comment)
-  return df 
 
-df = apply_subjectivity_on_all_comments(df)
 
 def find_polarity_of_single_comment(text):
    return  TextBlob(text).sentiment.polarity
@@ -114,57 +107,39 @@ negative_comments = 0
 neutral_comments = 0
 
 def print_positive_comments():
-  print('Printing positive comments:\n')
   sortedDF = df.sort_values(by=['Polarity']) 
   for i in range(0, sortedDF.shape[0] ):
     if( sortedDF['Analysis'][i] == 'Positive'):
       global positive_comments
       positive_comments += 1
-      print(str(i+1) + '> '+ sortedDF['Comments'][i])
-      print()
 print_positive_comments()
 
 def print_negative_comments():
-  print('Printing negative comments:\n')
   sortedDF = df.sort_values(by=['Polarity']) 
   for i in range(0, sortedDF.shape[0] ):
     if( sortedDF['Analysis'][i] == 'Negative'):
       global negative_comments
       negative_comments += 1
-      print(str(i+1) + '> '+ sortedDF['Comments'][i])
-      print()
 print_negative_comments()
 
 
 def print_neutral_comments():
-  print('Printing neutral comments:\n')
   sortedDF = df.sort_values(by=['Polarity']) 
   for i in range(0, sortedDF.shape[0] ):
     if( sortedDF['Analysis'][i] == 'Neutral'):
       global neutral_comments
       neutral_comments += 1
-      print(str(i+1) + '> '+ sortedDF['Comments'][i])
-      print()
 print_neutral_comments()
 
-print(positive_comments, negative_comments, neutral_comments)
+Sum = positive_comments + negative_comments + neutral_comments
+positive_comments = (positive_comments/Sum)*100
 
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import text 
 import numpy as np
-# from wordcloud import WordCloud
-
-# def generate_word_clouds(df):
-#   allWords = ' '.join([twts for twts in df['Comments']])
-#   wordCloud = WordCloud(stopwords = text.ENGLISH_STOP_WORDS ,width=1000, height=600, random_state=21, max_font_size=110).generate(allWords)
-#   plt.imshow(wordCloud, interpolation="bilinear")
-#   plt.axis('off')
-#   plt.show()
-
-# generate_word_clouds(df)
 
 graph = np.array([positive_comments, negative_comments, neutral_comments])
-labels = [f"Positive {positive_comments}", f"Negative {negative_comments}", f"Neutral {neutral_comments}"]
+labels = [f"Positive {positive_comments}%", f"Negative {negative_comments}%", f"Neutral {neutral_comments}%"]
 
 plt.pie(graph, labels= labels)
 plt.show()
